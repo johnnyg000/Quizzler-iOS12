289d26cf1ae7aa65fa2cba916ecab088b5c45887
johnnyg000 <39979642+johnnyg000@users.noreply.github.com>
2019-02-25 08:50:00 +0200

    Initial commit

---
+++ Quizzler-iOS12/Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Quizzler-Bridging-Header.h
+//  Quizzler
+//
+//  Created by John Gillespie on 2019/01/14.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#ifndef Quizzler_Bridging_Header_h
+#define Quizzler_Bridging_Header_h
+
+
+#endif /* Quizzler_Bridging_Header_h */
+
+#import "ProgressHUD.h"
